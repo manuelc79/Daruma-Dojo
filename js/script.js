174274338f24ec9edd72cea435ecac78c4239616
Ziko <34587375+manuelc79@users.noreply.github.com>
2023-09-06 14:24:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+var principal__marco1= document.querySelector(".principal__marco1");
+
+function animateBars() {
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+
+    principal__marco1.classList.toggle("principal__marco1-active"); /*descomentar para activar el menu desplegable */
+}
