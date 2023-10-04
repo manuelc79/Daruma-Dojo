a995cf330b3f6637cb2ad98a3d40754c61b823c4
Ziko <34587375+manuelc79@users.noreply.github.com>
2023-10-04 09:12:16 -0300

    Add files via upload

--- js/galeria.js
+++ js/galeria.js
@@ -1,12 +1,11 @@
-const fullImgBox =document.getElementById("full-ImgBox"),
-fullImg = document.getElementById("fullImg");
+const fullImg = document.getElementById("fullImg");
 
 function openFullImg(reference) {
-    fullImgBox.style.display="flex";
+    
     fullImg.src = reference;
 }
-
+/*
 function closeImg() {
     fullImgBox.style.display ="none";
 }
-
+*/
